7edb959e4b1c197e2022fd669db5514904ce5b4f
CassandraB670 <cassandra.boudier@gmail.com>
2023-12-13 00:19:12 +0100

    file with script that starts a Flask application

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Script that starts a Flask application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnh():
+    """Displays 'Hello HBNB:'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """Displays 'C' followed by the value of the variable"""
+    new_text = text.replace("_", " ")
+    return f"C {new_text}"
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text):
+    """Displays 'Python' followed by the value of the variable"""
+    new_text = text.replace("_", " ")
+    return f"Python {new_text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

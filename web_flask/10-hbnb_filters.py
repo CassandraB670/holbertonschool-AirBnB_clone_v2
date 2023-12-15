752f378cbbaf5dacfcfd12d618a8501ab840b01b
CassandraB670 <cassandra.boudier@gmail.com>
2023-12-15 09:18:45 +0100

    change all the file

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-""" Script that starts a Flask application"""
+""" Script that starts a Flask web application """
 
 from flask import Flask, render_template
 from models import storage
-from sqlalchemy.orm import scoped_session
 from models.state import State
 from models.amenity import Amenity
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_session(exception=None):
-    """Remove the current SQLAlchemy Session"""
-    storage.close()
-
-
-@app.route("/hbnb_filters", strict_slashes=False)
+@app.route('/hbnb_filters', strict_slashes=False)
 def hbnb_filters():
-    """display a HTML page"""
-    states = storage.all(State).values()
-    amenities = storage.all(Amenity).values()
+    """ Display states and amenities """
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
     return render_template('10-hbnb_filters.html', states=states,
                            amenities=amenities)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.teardown_appcontext
+def close_session(self):
+    """ Closes the session after each request """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
